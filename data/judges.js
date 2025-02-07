895c817d7e1f76688d65de152b81542bc95c188d
Andy Pak <im.andy.pak@gmail.com>
2025-02-06 19:02:52 -0500

    Update judges info

---
+++ data/judges.js
@@ -0,0 +1,20 @@
+export const judges = [
+    {
+      name: 'Shaaf Farooq',
+      image: '/shaaf_farooq.jpg',
+      background: 'Program Manager and VC Fellow at McMaster University',
+      bio: 'With expertise in scaling health tech and AI startups, Shaaf combines certifications in PMP and Lean Six Sigma with a passion for health informatics, innovation, and venture creation.'
+    },
+    {
+      name: 'Dua Saqib',
+      image: '/dua_saqib.jpg',
+      background: 'Program Administrator at The Clinic, McMaster University',
+      bio: 'Dua excels in fostering health innovation programs, with a strong focus on program development, data analysis, and strategic innovation. She has previous experience working on AI/ML projects at Bell Canada.'
+    },
+    {
+      name: 'Mann Parikh',
+      image: '/mann_parikh.jpg',
+      background: 'Founder & CEO of NerView Surgical',
+      bio: 'Mann is an innovator in non-invasive surgical technologies, with a strong background in health sciences and biomedical innovation. His work is driven by a dedication to advancing healthcare technology.'
+    }
+  ];
